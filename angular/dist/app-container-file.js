09f3d1ad8b3bffb3e87f5935edcc673e7ec1fff2
Paul Adams <adamspe@gmail.com>
2017-01-26 16:09:55 -0600

    adjust to odata-resource-file 1.0.0 and start angular.

---
+++ angular/dist/app-container-file.js
@@ -0,0 +1,33 @@
+/*
+ * app-container-file
+ * Version: 1.0.0 - 2017-01-23
+ */
+
+angular.module('app-container-file',[
+    'app-container-common',
+    'templates-app-container-file',
+    'odata-resource-file'
+])
+.provider('$appServiceFile',[function(){
+    // wrap the $fileResource provider to impose a configurable apiRoot
+    // as is done for $appService
+    this.apiRoot = '/api/v1/';
+    this.$get = ['$fileResource',function($fileResource){
+        var apiRoot = this.apiRoot;
+        return function(path) {
+            return $fileResource(apiRoot+path);
+        };
+    }];
+}])
+.service('Img',['$appServiceFile',function($appServiceFile){
+    return $appServiceFile('file/img/:id');
+}])
+.service('File',['$appServiceFile',function($appServiceFile){
+    return $appServiceFile('file/file/:id');
+}])
+.service('FileMeta',['$appServiceFile',function($appServiceFile){
+    return $appServiceFile('file/fileMeta/:id');
+}]);
+
+angular.module('templates-app-container-file', []);
+
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 var AppContainer = require('app-container'),
     conf = require('app-container-conf'),
-    prefix = (conf.get('resources:$apiRoot')||'/api/v1/'),
+    prefix = (conf.get('resources:$apiRoot')||'/api/v1/')+'file/',
     odataFile = require('odata-resource-file'),
     Resource = require('odata-resource'),
     _ = require('lodash'),
     appModule = {
         File: odataFile.File,
         Image: odataFile.img({ collection: 'Image', formats: (conf.get('resources:image:formats')||[]) }),
-        FileMeta: require('./FileMeta'),
         appInit: function(app) {
-            appModule.fileResource = odataFile.fileResource({
+            var file = appModule.fileResource = odataFile.fileResource({
                 rel: prefix+'file',
                 tmp: 'tmp/'
             },app);
-
-            appModule.fileMetaResource = new Resource({
-                rel: prefix+'fileMeta',
-                model: appModule.FileMeta
-            }).instanceLink('file',{
-                otherSide: appModule.fileResource,
-                key: '_file'
-            });
-            appModule.fileMetaResource.initRouter(app);
+            function addMeta(superFunc) {
+                return function(req,res){
+                    var meta = req.body.metadata;
+                    if(meta && typeof(meta) === 'string') {
+                        meta = JSON.parse(meta);
+                    }
+                    meta = meta||{};
+                    req.body.metadata = _.extend(meta,{
+                        // store the user id as a string (not id) since File's schema
+                        // is not strict otherwise we can't find like $filter=metadata._user eq '...'
+                        _user: req.user._id.toString()
+                    });
+                    superFunc.apply(this,arguments);
+                };
+            }
+            file.create = addMeta(file.create);
+            file.update = addMeta(file.update);
 
             appModule.imageResource = odataFile.imgResource({
                 rel: prefix+'image',
